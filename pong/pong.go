e7026a318f326def7e59f9cf0e25fdcbdbc56ea8
tomasz <tomrowicki@gmail.com>
2020-08-29 18:17:02 +0200

    bufixes, scores, controller suppor

--- pong/pong.go
+++ pong/pong.go
@@ -3,10 +3,54 @@ package main
 import (
 	"fmt"
 	"github.com/veandco/go-sdl2/sdl"
+	"math"
+	"time"
 )
 
-const winWidth = 800
-const winHeight = 600
+const winWidth, winHeight = 800, 600
+
+// start of unique and bizarre golang representation of enums
+type gameState int
+
+const (
+	start gameState = iota
+	play
+)
+
+// end of enum declaration
+
+var state = start
+
+var nums = [][]byte{
+	{
+		1, 1, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 1, 1,
+	},
+	{
+		1, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+		1, 0, 0,
+		1, 1, 1,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		0, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+	},
+}
 
 type color struct {
 	r, g, b byte
@@ -19,79 +63,126 @@ type pos struct {
 type ball struct {
 	// inherits pos props
 	pos
-	radius int
+	radius float32
 	// v stands for velocity
 	xv    float32
 	yv    float32
 	color color
 }
 
+func drawNumber(pos pos, color color, size int, num int, pixels []byte) {
+	startX := int(pos.x) - (size*3)/2
+	startY := int(pos.y) - (size*5)/2
+
+	for i, v := range nums[num] {
+		if v == 1 {
+			for y := startY; y < startY+size; y++ {
+				for x := startX; x < startX+size; x++ {
+					setPixel(x, y, color, pixels)
+				}
+			}
+		}
+		startX += size
+		if (i+1)%3 == 0 {
+			startY += size
+			startX -= size * 3
+		}
+	}
+}
+
 func (ball *ball) draw(pixels []byte) {
 	for y := -ball.radius; y < ball.radius; y++ {
 		for x := -ball.radius; x < ball.radius; x++ {
 			if x*x+y*y < ball.radius*ball.radius {
-				setPixel(int(ball.x)+x, int(ball.y)+y, ball.color, pixels)
+				setPixel(int(ball.x+x), int(ball.y+y), ball.color, pixels)
 			}
 		}
 	}
 }
 
-func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle) {
-	ball.x += ball.xv
-	ball.y += ball.yv
+func getCenter() pos {
+	return pos{float32(winWidth / 2), float32(winHeight / 2)}
+}
+
+func (ball *ball) update(leftPaddle *paddle, rightPaddle *paddle, elapsedTime float32) {
+	ball.x += ball.xv * elapsedTime
+	ball.y += ball.yv * elapsedTime
 
 	// handling collisions
-	if int(ball.y)-ball.radius < 0 || int(ball.y)+ball.radius > winHeight {
+	if ball.y-ball.radius < 0 || ball.y+ball.radius > winHeight {
 		ball.yv = -ball.yv
 	}
 
-	if ball.x < 0 || ball.x > winWidth {
-		ball.x = 300
-		ball.y = 300
+	if ball.x < 0 {
+		rightPaddle.score++
+		ball.pos = getCenter()
+		state = start
+	} else if ball.x > winWidth {
+		leftPaddle.score++
+		ball.pos = getCenter()
+		state = start
 	}
 
-	if ball.x < leftPaddle.x+float32(leftPaddle.w/2) {
-		if ball.y > leftPaddle.y-float32(leftPaddle.h)/2 && ball.y < leftPaddle.y+float32(leftPaddle.h)/2 {
+	if ball.x - ball.radius < leftPaddle.x+leftPaddle.w/2 {
+		if ball.y > leftPaddle.y-leftPaddle.h/2 && ball.y < leftPaddle.y+leftPaddle.h/2 {
 			ball.xv = -ball.xv
+			ball.x = leftPaddle.x + leftPaddle.w/2.0 + ball.radius
 		}
 	}
 
-	if ball.x > rightPaddle.x-float32(rightPaddle.w) {
-		if ball.y > rightPaddle.y-float32(rightPaddle.h)/2 && ball.y < rightPaddle.y+float32(rightPaddle.h)/2 {
+	if ball.x + ball.radius > rightPaddle.x-rightPaddle.w/2 {
+		if ball.y > rightPaddle.y-rightPaddle.h/2 && ball.y < rightPaddle.y+rightPaddle.h/2 {
 			ball.xv = -ball.xv
+			ball.x = rightPaddle.x - rightPaddle.w/2 - ball.radius
 		}
 	}
 }
 
 type paddle struct {
 	pos
-	w     int
-	h     int
+	w     float32
+	h     float32
+	speed float32
+	score int
 	color color
 }
 
-func (paddle *paddle) draw(pixels []byte) {
-	startX := int(paddle.x) - paddle.w/2
-	startY := int(paddle.y) - paddle.h/2
+// calculates a value between two extremes, given its relative distance to them(?) expressed in percent
+func lerp(a float32, b float32, pct float32) float32 {
+	return a + pct*(b-a)
+}
 
-	for y := 0; y < paddle.h; y++ {
-		for x := 0; x < paddle.w; x++ {
+func (paddle *paddle) draw(pixels []byte) {
+	startX := int(paddle.x - paddle.w/2)
+	startY := int(paddle.y - paddle.h/2)
+
+	for y := 0; y < int(paddle.h); y++ {
+		for x := 0; x < int(paddle.w); x++ {
 			setPixel(startX+x, startY+y, paddle.color, pixels)
 		}
 	}
+
+	numX := lerp(paddle.x, getCenter().x, 0.2)
+	drawNumber(pos{numX, 35}, paddle.color, 10, paddle.score, pixels)
 }
 
-func (paddle *paddle) update(keyState []uint8) {
+func (paddle *paddle) update(keyState []uint8, controllerAxis int16, elapsedTime float32) {
 	if keyState[sdl.SCANCODE_UP] != 0 {
-		paddle.y -= 5
+		paddle.y -= paddle.speed * elapsedTime
 	}
 
 	if keyState[sdl.SCANCODE_DOWN] != 0 {
-		paddle.y += 5
+		paddle.y += paddle.speed * elapsedTime
+	}
+
+	if math.Abs(float64(controllerAxis)) > 150 {
+		// what percentage is controller moved; 32767 is the max value
+		pct := float32(controllerAxis) / 32767.0
+		paddle.y += paddle.speed * pct * elapsedTime
 	}
 }
 
-func (paddle *paddle) aiUpdate(ball *ball) {
+func (paddle *paddle) aiUpdate(ball *ball, elapsedTime float32) {
 	paddle.y = ball.y
 }
 
@@ -132,18 +223,29 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	defer renderer.Destroy()
+	defer tex.Destroy()
+
+	var controllerHandlers []*sdl.GameController
+	for i:=0; i<sdl.NumJoysticks(); i++ {
+		controllerHandlers = append(controllerHandlers, sdl.GameControllerOpen(i))
+		defer controllerHandlers[i].Close()
+	}
 
 	// 4 bytes per pixel - for colours
 	pixels := make([]byte, winWidth*winHeight*4)
 
-	player1 := paddle{pos{50, 100}, 20, 100, color{255, 255, 255}}
-	player2 := paddle{pos{winWidth - 50, 100}, 20, 100, color{255, 255, 255}}
-	ball := ball{pos{300, 300}, 20, 7, 7, color{255, 255, 255}}
+	player1 := paddle{pos{50, 100}, 20, 100, 300, 0, color{255, 255, 255}}
+	player2 := paddle{pos{winWidth - 50, 100}, 20, 100, 300, 0, color{255, 255, 255}}
+	ball := ball{pos{300, 300}, 20, 400, 400, color{255, 255, 255}}
 
 	keyState := sdl.GetKeyboardState()
 
+	var frameStart time.Time
+	var elapsedTime float32
+	var controllerAxis int16
+
 	for {
+		frameStart = time.Now()
 		// allows for quitting with the window's x button
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
@@ -151,20 +253,41 @@ func main() {
 				return
 			}
 		}
-		clear(pixels)
 
+		for _, controller := range controllerHandlers {
+			if controller != nil {
+				controllerAxis = controller.Axis(sdl.CONTROLLER_AXIS_LEFTY)
+			}
+		}
+
+		if state == play {
+			player1.update(keyState, controllerAxis, elapsedTime)
+			player2.aiUpdate(&ball, elapsedTime)
+			ball.update(&player1, &player2, elapsedTime)
+		} else if state == start {
+			if keyState[sdl.SCANCODE_SPACE] != 0 {
+				if player1.score == 3 || player2.score == 3 {
+				player1.score = 0
+				player2.score = 0
+				}
+				state = play
+			}
+		}
+
+		clear(pixels)
 		player1.draw(pixels)
 		player2.draw(pixels)
 		ball.draw(pixels)
-
-		player1.update(keyState)
-		ball.update(&player1, &player2)
-		player2.aiUpdate(&ball)
 
 		tex.Update(nil, pixels, winWidth*4)
 		renderer.Copy(tex, nil, nil)
 		renderer.Present()
 
-		sdl.Delay(16)
+		elapsedTime = float32(time.Since(frameStart).Seconds())
+		// in case too small amount of time passes before each subsequent iteration, 0.005 limits to 200fps
+		if elapsedTime < 0.005 {
+			sdl.Delay(5 - uint32(elapsedTime * 1000.0))
+			elapsedTime = float32(time.Since(frameStart).Seconds())
+		}
 	}
 }
