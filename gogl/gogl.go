342b53afab7245730bbf462ad3153a77275cad68
tomasz <tomrowicki@gmail.com>
2020-10-26 13:35:30 +0100

    OpenGL helper functions

---
+++ gogl/gogl.go
@@ -0,0 +1,153 @@
+package gogl
+
+import (
+	"errors"
+	"fmt"
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"io/ioutil"
+	"strings"
+	"time"
+)
+
+type ShaderID uint32
+type ProgramID uint32
+type VAOID uint32
+type VBOID uint32
+
+func GetVersion() string {
+	return gl.GoStr(gl.GetString(gl.VERSION))
+}
+
+type programInfo struct {
+	vertPath string
+	fragPath string
+
+	modified time.Time
+}
+
+var loadedShaders []programInfo
+
+func CheckShadersForChanges() {
+	//for _, shaderInfo := range loadedShaders {
+	//	file, err := os.Stat(shaderInfo.path)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//	modTime := file.ModTime()
+	//	// check if greater than?
+	//	if !modTime.Equal(shaderInfo.modified) {
+	//		fmt.Println("shader modified")
+	//	}
+	//}
+}
+
+func LoadShader(path string, shaderType uint32) (ShaderID, error) {
+	shaderFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return 0, err
+	}
+	shaderFileStr := string(shaderFile)
+	shaderId, err := CreateShader(shaderFileStr, shaderType)
+	if err != nil {
+		return 0, err
+	}
+
+	//file, err := os.Stat(path)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//modTime := file.ModTime()
+	//loadedShaders = append(loadedShaders, programInfo{path,modTime})
+	return shaderId, nil
+}
+
+func CreateShader(shaderSource string, shaderType uint32) (ShaderID, error) {
+	shaderId := gl.CreateShader(shaderType)
+
+	//The error around halfway with shader compiling is due to not adding a null terminator to the shader source strings. You can fix this by adding:
+	//+ "\x00"
+	//after the final back tick on the shader strings.
+	shaderSource = shaderSource + "\x00"
+	csource, free := gl.Strs(shaderSource)
+	gl.ShaderSource(shaderId, 1, csource, nil)
+	free()
+	gl.CompileShader(shaderId)
+	var status int32
+	gl.GetShaderiv(shaderId, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE { // compilation did not succeed...
+		var logLength int32
+		gl.GetShaderiv(shaderId, gl.INFO_LOG_LENGTH, &logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shaderId, logLength, nil, gl.Str(log))
+		fmt.Println("failed to compile shader:\n" + log)
+		return 0, errors.New("failed to compile shader: " + log)
+	}
+	return ShaderID(shaderId), nil
+}
+
+func CreateProgram(vertPath string, fragPath string) (ProgramID, error) {
+	vert, err := LoadShader(vertPath, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+	frag, err := LoadShader(fragPath, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+	shaderProgram := gl.CreateProgram()
+	gl.AttachShader(shaderProgram, uint32(vert))
+	gl.AttachShader(shaderProgram, uint32(frag))
+	gl.LinkProgram(shaderProgram)
+	var success int32
+	gl.GetProgramiv(shaderProgram, gl.LINK_STATUS, &success)
+
+	if success == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(shaderProgram,gl.INFO_LOG_LENGTH,&logLength)
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(shaderProgram,logLength, nil,gl.Str(log))
+		return 0, errors.New("failed to link program:\n" + log)
+	}
+	gl.DeleteShader(uint32(vert))
+	gl.DeleteShader(uint32(frag))
+
+	// TODO
+	//file, err := os.Stat(path)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//modTime := file.ModTime()
+	//loadedShaders = append(loadedShaders, programInfo{path,modTime})
+
+	return ProgramID(shaderProgram), nil
+}
+
+func GenBindBuffer(target uint32) VBOID {
+	var VBO uint32 // Vertex Buffer Object
+	gl.GenBuffers(1, &VBO)
+	gl.BindBuffer(target, VBO)
+	return VBOID(target)
+}
+
+func GenBindVertexArray() VAOID {
+	var VAO uint32
+	gl.GenVertexArrays(1, &VAO)
+	gl.BindVertexArray(VAO)
+	return VAOID(VAO)
+}
+
+func BindVertexArray(id VAOID) {
+	gl.BindVertexArray(uint32(id))
+}
+
+func BufferDataFloat(target uint32, data []float32, usage uint32) {
+	gl.BufferData(target, len(data)*4, gl.Ptr(data), usage) // 4, as there are 4 bytes making a float32 value
+}
+
+func UnbindVertexArray() {
+	gl.BindVertexArray(0)
+}
+
+func UseProgram (id ProgramID) {
+	gl.UseProgram(uint32(id))
+}
--- hellotriangle/hellotriangle.go
+++ hellotriangle/hellotriangle.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"gameswithgo/gogl"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/veandco/go-sdl2/sdl"
-	"strings"
 )
 
 // https://learnopengl.com/ - nice OpenGL learning source (requires C knowledge)
@@ -31,94 +31,27 @@ func main() {
 	defer window.Destroy()
 
 	gl.Init()
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println(version)
 
-	vertexShaderSource :=
-		`#version 330 core
-		layout (location = 0) in vec3 aPos;
+	fmt.Println(gogl.GetVersion())
 
-		void main()
-		{
-			gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);
-		}` + "\x00"
-
-	//The error around halfway with shader compiling is due to not adding a null terminator to the shader source strings. You can fix this by adding:
-	//+ "\x00"
-	//after the final back tick on the shader strings.
-
-	vertexShader := gl.CreateShader(gl.VERTEX_SHADER)
-	csource, free := gl.Strs(vertexShaderSource)
-	gl.ShaderSource(vertexShader, 1, csource, nil)
-	free()
-	gl.CompileShader(vertexShader)
-	var status int32
-	gl.GetShaderiv(vertexShader,gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE { // compilation did not succeed...
-		var logLength int32
-		gl.GetShaderiv(vertexShader,gl.INFO_LOG_LENGTH,&logLength)
-		log := strings.Repeat("\x00", int(logLength+1)) // FIXME better way shown in YT comment
-		gl.GetShaderInfoLog(vertexShader,logLength, nil,gl.Str(log))
-		panic("failed to compile vertex shader:\n" + log)
+	shaderProgram, err := gogl.CreateProgram("hellotriangle/shaders/hello.vert", "hellotriangle/shaders/hello.frag")
+	if err != nil {
+		panic(err)
 	}
-
-	fragmentShaderSource :=
-		`# version 330 core
-		out vec4 FragColor;
-		
-		void main()
-		{
-			FragColor = vec4(1.0f,0.5f,0.2f,1.0f);
-		}` + "\x00"
-
-	fragmentShader := gl.CreateShader(gl.FRAGMENT_SHADER)
-	csource, free = gl.Strs(fragmentShaderSource)
-	gl.ShaderSource(fragmentShader, 1, csource, nil)
-	free()
-	gl.CompileShader(fragmentShader)
-	gl.GetShaderiv(fragmentShader,gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE { // compilation did not succeed...
-		var logLength int32
-		gl.GetShaderiv(fragmentShader,gl.INFO_LOG_LENGTH,&logLength)
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(fragmentShader,logLength, nil,gl.Str(log))
-		panic("failed to compile fragment shader:\n" + log)
-	}
-
-	shaderProgram := gl.CreateProgram()
-	gl.AttachShader(shaderProgram, vertexShader)
-	gl.AttachShader(shaderProgram, fragmentShader)
-	gl.LinkProgram(shaderProgram)
-	var success int32
-	gl.GetProgramiv(shaderProgram, gl.LINK_STATUS, &success)
-	if success == gl.FALSE {
-		var logLength int32
-		gl.GetProgramiv(shaderProgram,gl.INFO_LOG_LENGTH,&logLength)
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetProgramInfoLog(shaderProgram,logLength, nil,gl.Str(log))
-		panic("failed to link program:\n" + log)
-	}
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
-
 
 	vertices := []float32{
 		-0.5, -0.5, 0.0,
 		0.5, -0.5, 0.0,
 		0.0, 0.5, 0.0}
 
-	var VBO uint32 // Vertex Buffer Object
-	gl.GenBuffers(1, &VBO)
-	gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-	var VAO uint32
-	gl.GenVertexArrays(1, &VAO)
-	gl.BindVertexArray(VAO)
+	gogl.GenBindBuffer(gl.ARRAY_BUFFER)
+	VAO := gogl.GenBindVertexArray()
 
-	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW) // 4, as there are 4 bytes making a float32 value
+	gogl.BufferDataFloat(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
 
-	gl.VertexAttribPointer(0,3,gl.FLOAT,false,3*4,nil)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
 	gl.EnableVertexAttribArray(0)
-	gl.BindVertexArray(0)
+	gogl.UnbindVertexArray()
 
 	for {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -127,13 +60,15 @@ func main() {
 				return
 			}
 		}
-		gl.ClearColor(0,0,0,0)
+		gl.ClearColor(0, 0, 0, 0)
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
-		gl.UseProgram(shaderProgram)
-		gl.BindVertexArray(VAO)
-		gl.DrawArrays(gl.TRIANGLES,0,3)
+		gogl.UseProgram(shaderProgram)
+		gogl.BindVertexArray(VAO)
+		gl.DrawArrays(gl.TRIANGLES, 0, 3)
 
 		window.GLSwap()
+
+		gogl.CheckShadersForChanges()
 	}
 }
